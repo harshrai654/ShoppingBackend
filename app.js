93c24fd765ee3255aab41aaa02e18de19b02d64f
harshrai654 <harshraitth2@gmail.com>
2021-02-10 00:52:43 +0530

    Configured mongo connection

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const indexRouter = require("./routes/index");
 const app = express();
 const port = process.env.PORT || 3001;
+const client = require("./initDB");
+
+//Connection yo Database
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to MongoDB cluster successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //Middlewares
 app.use(express.json());
---
+++ initDB.js
@@ -0,0 +1,7 @@
+const { MongoClient } = require("mongodb");
+
+const uri = `mongodb+srv://spider:${process.env.DB_PASS}@shoppingcluster.ogvk4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+module.exports = client;
