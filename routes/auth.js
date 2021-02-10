86c477dfbb83c400cf33af3bef5054ee410d6329
harshrai654 <harshraitth2@gmail.com>
2021-02-10 22:44:32 +0530

    Auth routes with JWT implemented

---
+++ routes/auth.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const router = express.Router();
+const client = require("../initDB");
+const jwt = require("jsonwebtoken");
+
+/* GET home page. */
+router.post("/login", function (req, res) {
+  const { email, pass } = req.body;
+
+  if (email && pass) {
+    const userCollection = client
+      .db(process.env.DB_NAME)
+      .collection(process.env.USER_COLL);
+
+    //Verifying credentials in DB
+
+    userCollection
+      .findOne({ email, pass })
+      .then((dbData) => {
+        if (dbData) {
+          const payload = {
+            name: dbData.name,
+            email: dbData.email,
+            _id: dbData._id,
+          };
+          //Creating token
+          const secretKey = process.env.SECRET;
+
+          jwt.sign(payload, secretKey, { expiresIn: "7d" }, (err, token) => {
+            if (err) res.sendStatus(500);
+            else {
+              console.log(`${payload._id} Logged in`);
+              res.json({
+                token,
+              });
+            }
+          });
+        } else {
+          console.log("Login failed");
+          res.sendStatus(403);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
+
+router.post("/sellerlogin", function (req, res) {
+  const { email, pass } = req.body;
+
+  if (email && pass) {
+    const sellerCollection = client
+      .db(process.env.DB_NAME)
+      .collection(process.env.SELL_COLL);
+
+    //Verifying credentials in DB
+
+    sellerCollection
+      .findOne({ email, pass })
+      .then((dbData) => {
+        if (dbData) {
+          const payload = {
+            name: dbData.name,
+            email: dbData.email,
+            _id: dbData._id,
+          };
+          //Creating token
+          const secretKey = process.env.SECRET;
+
+          jwt.sign(payload, secretKey, { expiresIn: "7d" }, (err, token) => {
+            if (err) res.sendStatus(500);
+            else {
+              console.log(`${payload._id} Logged in`);
+              res.json({
+                token,
+              });
+            }
+          });
+        } else {
+          console.log("Login failed");
+          res.sendStatus(403);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+});
+
+module.exports = router;
